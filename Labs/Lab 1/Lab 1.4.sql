273e1ccd721584541d507807faa882ee733c68b6
ShadowLim <vadimsh853@gmail.com>
2017-02-12 23:33:45 +0300

    Added solution to 4th and last point in 1st lab

---
+++ Labs/Lab 1/Lab 1.4.sql
@@ -0,0 +1,14 @@
+--Выбрать информацию (код покупателя, код товара, даты заказов) о случаях, 
+--	когда один и тот же покупатель заказывал один и тот же товар с интервалом менее недели.
+
+select distinct a.customer_id, a.product_id, a.order_date
+	from 
+		--Making 2 same tables
+		(select customer_id, product_id, order_date, SALES_ORDER.order_id
+			from SALES_ORDER, ITEM where SALES_ORDER.order_id = ITEM.order_id) a,
+		(select customer_id, product_id, order_date, SALES_ORDER.order_id
+			from SALES_ORDER, ITEM where SALES_ORDER.order_id = ITEM.order_id) b
+		--Joinig them so that corteges contain two orders from one customer and aimed on one
+		--	product. Then choosing only different purchases with datediff less then week.
+	where a.customer_id = b.customer_id and a.product_id = b.product_id and 
+		a.order_id != b.order_id and abs( datediff(d, a.order_date, b.order_date)) < 7
