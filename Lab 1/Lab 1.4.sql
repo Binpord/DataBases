9588aff46d5a98f25985cc97fd0fb43ee282f31a
ShadowLim <vadimsh853@gmail.com>
2017-02-15 11:51:55 +0300

    Made script a little lesser and a little easier to read

--- Lab 1/Lab 1.4.sql
+++ Lab 1/Lab 1.4.sql
@@ -1,15 +1,15 @@
 --Выбрать информацию (код покупателя, код товара, даты заказов) о случаях, 
 --	когда один и тот же покупатель заказывал один и тот же товар с интервалом менее недели.
 
-select distinct a.customer_id, a.product_id, a.order_date
+--Using with as we need 2 exact same tables
+with tmp_table(customer_id, product_id, order_date, order_id) as
+	(select customer_id, product_id, order_date, SALES_ORDER.order_id
+			from SALES_ORDER join ITEM on SALES_ORDER.order_id = ITEM.order_id)
+
+select distinct t1.customer_id, t1.product_id, t1.order_date
 	from 
-		--Making 2 exact same tables
-		(select customer_id, product_id, order_date, SALES_ORDER.order_id
-			from SALES_ORDER join ITEM on SALES_ORDER.order_id = ITEM.order_id) a
-			join
-		(select customer_id, product_id, order_date, SALES_ORDER.order_id
-			from SALES_ORDER join ITEM on SALES_ORDER.order_id = ITEM.order_id) b
-			on a.customer_id = b.customer_id and a.product_id = b.product_id
+		tmp_table t1 join tmp_table t2
+			on t1.customer_id = t2.customer_id and t1.product_id = t2.product_id
 		--Joinig them so that corteges contain two orders from one customer and aimed on one
 		--	product. Then choosing only different purchases with datediff less then week.
-	where a.order_id != b.order_id and abs( datediff(d, a.order_date, b.order_date)) < 7
+	where t1.order_id != t2.order_id and abs( datediff(d, t1.order_date, t2.order_date)) < 7
