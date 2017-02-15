2eba244985af2c7ec4e5c90d2c026e8c8e577246
ShadowLim <vadimsh853@gmail.com>
2017-02-15 11:11:46 +0300

    Changed style of joinig tables

--- Lab 1/Lab 1.4.sql
+++ Lab 1/Lab 1.4.sql
@@ -3,12 +3,13 @@
 
 select distinct a.customer_id, a.product_id, a.order_date
 	from 
-		--Making 2 same tables
+		--Making 2 exact same tables
 		(select customer_id, product_id, order_date, SALES_ORDER.order_id
-			from SALES_ORDER, ITEM where SALES_ORDER.order_id = ITEM.order_id) a,
+			from SALES_ORDER join ITEM on SALES_ORDER.order_id = ITEM.order_id) a
+			join
 		(select customer_id, product_id, order_date, SALES_ORDER.order_id
-			from SALES_ORDER, ITEM where SALES_ORDER.order_id = ITEM.order_id) b
+			from SALES_ORDER join ITEM on SALES_ORDER.order_id = ITEM.order_id) b
+			on a.customer_id = b.customer_id and a.product_id = b.product_id
 		--Joinig them so that corteges contain two orders from one customer and aimed on one
 		--	product. Then choosing only different purchases with datediff less then week.
-	where a.customer_id = b.customer_id and a.product_id = b.product_id and 
-		a.order_id != b.order_id and abs( datediff(d, a.order_date, b.order_date)) < 7
+	where a.order_id != b.order_id and abs( datediff(d, a.order_date, b.order_date)) < 7
