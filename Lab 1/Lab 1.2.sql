38b954fdbc412f1fe87718d4bb9fa2d53f4665a7
ShadowLim <vadimsh853@gmail.com>
2017-02-28 21:34:22 +0300

    git status shows that it was modified)

--- Lab 1/Lab 1.2.sql
+++ Lab 1/Lab 1.2.sql
@@ -1,8 +1,8 @@
 --Выбрать названия и города тех отделов, которым приходится вести дела с покупателями 
 --	из Техаса (TX)
 
-select DEPARTMENT.[name] as 'Название отдела', [regional_group] as 'Город' from
+select MAX(DEPARTMENT.[name]) as 'Название отдела', [regional_group] as 'Город' from
 	((CUSTOMER join EMPLOYEE on EMPLOYEE.employee_id = CUSTOMER.salesperson_id) join 
 		DEPARTMENT on EMPLOYEE.department_id = DEPARTMENT.department_id) join
 			LOCATION on LOCATION.location_id = DEPARTMENT.location_id
-	where state = 'TX' group by DEPARTMENT.[name], [regional_group];
+	where state = 'TX' group by DEPARTMENT.department_id, [regional_group];
