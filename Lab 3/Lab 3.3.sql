e38b3a022baa346d31d979b3b5c10a4b1a36efd7
ShadowLim <vadimsh853@gmail.com>
2017-03-06 13:27:42 +0300

    Cured the solution

--- Lab 3/Lab 3.3.sql
+++ Lab 3/Lab 3.3.sql
@@ -1,10 +1,16 @@
 --Select most popular tariff for every customer
 
-WITH TMP(customer, connection, tariff) as (
-SELECT CUSTOMER.name, COUNT(CONNECTION.tariff_id), TARIFF.name
-	FROM CUSTOMER JOIN CONNECTION ON CUSTOMER.customer_id = CONNECTION.customer_id
-	JOIN TARIFF ON CONNECTION.tariff_id = TARIFF.tariff_id
-	group by CUSTOMER.name, CONNECTION.tariff_id, TARIFF.name
+WITH TARIFF_PER_CUSTOMER(customer_id, customer, connection, tariff) AS (
+	SELECT CUSTOMER.customer_id, CUSTOMER.name, COUNT(CONNECTION.tariff_id), TARIFF.name
+		FROM CUSTOMER JOIN CONNECTION ON CUSTOMER.customer_id = CONNECTION.customer_id
+		JOIN TARIFF ON CONNECTION.tariff_id = TARIFF.tariff_id
+		group by CUSTOMER.customer_id, CUSTOMER.name, CONNECTION.tariff_id, TARIFF.name
+	),
+	MAX_TARIFF(customer_id, connection) AS (
+	SELECT customer_id, MAX(connection) FROM TARIFF_PER_CUSTOMER
+		GROUP BY customer_id
 	)
-SELECT TOP(1) customer, tariff FROM TMP
-	GROUP BY customer, tariff;
+SELECT TARIFF_PER_CUSTOMER.customer, tariff
+	FROM MAX_TARIFF LEFT JOIN TARIFF_PER_CUSTOMER 
+	ON MAX_TARIFF.customer_id = TARIFF_PER_CUSTOMER.customer_id 
+	AND TARIFF_PER_CUSTOMER.connection = MAX_TARIFF.connection;
