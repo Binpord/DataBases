518737257f29a58f6e985f97d9758a01b47935ea
Shiyanov Vadim <vadimsh853@gmail.com>
2017-04-04 22:27:47 +0300

    added try-catch for the error + added solution for the second part of the task

--- lab7/lab7.sql
+++ lab7/lab7.sql
@@ -6,6 +6,7 @@ DROP USER test;
 CREATE LOGIN test WITH PASSWORD = '111';
 CREATE USER test FOR LOGIN test;
 
+--FULL SELECT, INSERT and UPDATE rights
 GRANT SELECT, INSERT, UPDATE ON JOB TO test;
 
 BEGIN TRANSACTION;
@@ -20,7 +21,38 @@ UPDATE JOB
 	SET job_id = 420
 	WHERE [function] = N'машинист';
 
+BEGIN TRY
 SELECT * FROM EMPLOYEE;
+END TRY
+BEGIN CATCH
+PRINT 'experienced error: ';
+PRINT  ERROR_NUMBER();
+END CATCH;
+
+REVERT;
+ROLLBACK;
+
+--SELECT and UPDATE of chosen columns
+GRANT SELECT, UPDATE (job_id) ON EMPLOYEE TO test;
+
+BEGIN TRANSACTION;
+EXECUTE AS USER = 'test';
+
+SELECT * FROM EMPLOYEE;
+
+UPDATE EMPLOYEE
+	SET job_id = 400
+	WHERE last_name = N'Егоров' AND first_name = N'Владислав';
+
+BEGIN TRY
+UPDATE EMPLOYEE
+	SET manager_id = 507
+	WHERE last_name = N'Егоров' AND first_name = N'Владислав';
+END TRY
+BEGIN CATCH
+PRINT 'experienced error: ';
+PRINT  ERROR_NUMBER();
+END CATCH;
 
 REVERT;
 ROLLBACK;
