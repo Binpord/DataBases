79769bd8cd79e90f28418f71a1413bcb71aa7163
Shiyanov Vadim <vadimsh853@gmail.com>
2017-04-04 22:36:04 +0300

    slightly changed try-catch

--- lab7/lab7.sql
+++ lab7/lab7.sql
@@ -25,15 +25,17 @@ BEGIN TRY
 SELECT * FROM EMPLOYEE;
 END TRY
 BEGIN CATCH
-PRINT 'experienced error: ';
+PRINT 'experienced error trying to select from EMPLOYEE';
 PRINT  ERROR_NUMBER();
 END CATCH;
 
 REVERT;
 ROLLBACK;
 
+REVOKE SELECT, INSERT, UPDATE ON JOB TO test;
+
 --SELECT and UPDATE of chosen columns
-GRANT SELECT, UPDATE (job_id) ON EMPLOYEE TO test;
+GRANT SELECT, UPDATE(job_id) ON EMPLOYEE TO test;
 
 BEGIN TRANSACTION;
 EXECUTE AS USER = 'test';
@@ -50,7 +52,40 @@ UPDATE EMPLOYEE
 	WHERE last_name = N'Егоров' AND first_name = N'Владислав';
 END TRY
 BEGIN CATCH
-PRINT 'experienced error: ';
+PRINT 'experienced error trying to update manager_id:';
+PRINT  ERROR_NUMBER();
+END CATCH;
+
+REVERT;
+ROLLBACK;
+
+REVOKE SELECT, UPDATE(job_id) ON EMPLOYEE TO test;
+
+GRANT SELECT(job_id), UPDATE(job_id) ON EMPLOYEE TO test;
+
+BEGIN TRANSACTION;
+EXECUTE AS USER = 'test';
+
+SELECT job_id FROM EMPLOYEE;
+
+BEGIN TRY
+SELECT * FROM EMPLOYEE;
+END TRY
+BEGIN CATCH
+PRINT 'experienced error trying to select * from EMPLOYEE:';
+PRINT ERROR_NUMBER();
+END CATCH;
+
+UPDATE EMPLOYEE
+	SET job_id = 400;
+
+BEGIN TRY
+UPDATE EMPLOYEE
+	SET job_id = 400
+	WHERE last_name = N'Егоров' AND first_name = N'Владислав';
+END TRY
+BEGIN CATCH
+PRINT 'experienced error trying to update job_id with select:';
 PRINT  ERROR_NUMBER();
 END CATCH;
 
