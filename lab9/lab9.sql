0a736df3d31b8597300b3480f8913aadc1e93500
Shiyanov Vadim <vadimsh853@gmail.com>
2017-04-18 21:14:45 +0300

    added support for multiple delete

--- lab9/lab9.sql
+++ lab9/lab9.sql
@@ -1,24 +1,82 @@
 USE CELLULAR;
 
 GO
---Creating trigger, that defines policy that every customer should have at least one connection
 
+IF OBJECT_ID ('dbo.CONNECTIONS_TRACK','TR') IS NOT NULL
+   DROP TRIGGER dbo.CONNECTIONS_TRACK;
+
+GO
+
+--Creating trigger, that defines policy that every customer should have at least one connection
 CREATE TRIGGER CONNECTIONS_TRACK
 	ON CONNECTION
 	AFTER DELETE
 	AS
 	BEGIN
 		DECLARE @num_of_conns SMALLINT;
+		
+		-- "For each customer in deleted" implementation
+		DECLARE @deleted_count SMALLINT;
+		DECLARE @max_count SMALLINT;
+		DECLARE @cur_customer SMALLINT;
 
-		SET @num_of_conns = (SELECT COUNT(*) FROM CONNECTION WHERE customer_id = (SELECT customer_id FROM deleted));
+		SET @max_count = (SELECT COUNT(*) FROM deleted);
+		SET @deleted_count = 0;
 
-		IF @num_of_conns = 0
+		DECLARE @cp_deleted TABLE
+		(
+			customer_id SMALLINT, 
+			tariff_id SMALLINT, 
+			phone_number DECIMAL(10, 0), 
+			used_minutes SMALLINT, 
+			used_messages SMALLINT, 
+			used_internet SMALLINT, 
+			rate SMALLINT
+		);
+
+		INSERT INTO @cp_deleted
+			SELECT * FROM deleted;
+
+		WHILE(@deleted_count < @max_count)
 		BEGIN
-			PRINT 'Не удалось удалить подключение, так как оно единственное отвечающее данному пользователю.';
-			PRINT 'Перед удалением подключения удалите пользователя.';
-			ROLLBACK TRANSACTION;
+			SET @cur_customer = (SELECT TOP(1) customer_id FROM @cp_deleted);
+
+			SET @num_of_conns = (SELECT COUNT(*) FROM CONNECTION WHERE customer_id = @cur_customer);
+
+			IF @num_of_conns = 0
+			BEGIN
+				PRINT 'Не удалось выполнить DELETE, так как для одного или нескольких пользователей соединение является последним.';
+				PRINT 'Перед удалением такого подключения удалите пользователя.';
+				ROLLBACK TRANSACTION;
+				RETURN;
+			END
+			ELSE
+				PRINT 'Удаление успешно.'
+
+			DELETE FROM @cp_deleted
+				WHERE customer_id = @cur_customer;
+
+			SET @deleted_count += 1;
 		END
-		ELSE
-			PRINT 'Удаление успешно.'
 	END
+
 GO
+
+BEGIN TRANSACTION;
+
+SELECT CONNECTION.customer_id, name, COUNT(*) FROM CONNECTION JOIN CUSTOMER ON CONNECTION.customer_id = CUSTOMER.customer_id
+	GROUP BY CONNECTION.customer_id, name;
+
+SELECT 'Вадим Вадимов' AS name, phone_number FROM CONNECTION
+	WHERE customer_id = dbo.cstrid_from_name('Вадимов Вадим');
+
+SELECT 'Иванов Иван' AS name, phone_number FROM CONNECTION
+	WHERE customer_id = dbo.cstrid_from_name('Иванов Иван');
+
+DELETE FROM CONNECTION
+	WHERE phone_number = 9168581126;
+
+DELETE FROM CONNECTION
+	WHERE phone_number IN (9168581122, 9168581113);
+
+ROLLBACK TRANSACTION;
